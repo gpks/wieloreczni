3220a476e1726a38e9e207415e30e484c7d568ce
Malgorzata Ksionek <meksionek@gmail.com>
2014-10-19 11:55:29 +0200

    add game validation

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,10 +1,12 @@
 class Game < ActiveRecord::Base 
   has_and_belongs_to_many :users
 
-  validates :name, :presence => true, 
-  validates :description, :presence => true
-  validates :minplayer, :presence => true, inclusion: { in: 1..10 }
-  validates :maxplayer, :presence => true, inclusion: { in: 1..100 }
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :minplayer, presence: true
+  validates :minplayer, inclusion: { in: 1..10 }
+  validates :maxplayer, presence: true
+  validates :maxplayer, inclusion: { in: 1..100 }
   validates :minage, inclusion: { in: 0..18 }
 
 
